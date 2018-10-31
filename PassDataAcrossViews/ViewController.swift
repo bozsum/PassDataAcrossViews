9504b1c00967eb85240f2a10a199ede99d96d5fa
Bozsum/Bozsumwv <bozsumwv@gmail.com>
2018-10-31 21:40:08 +0700

    initial project

---
+++ PassDataAcrossViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataAcrossViews
+//
+//  Created by Jetsada Wareepot on 31/10/2561 BE.
+//  Copyright © 2561 Jetsada Wareepot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
