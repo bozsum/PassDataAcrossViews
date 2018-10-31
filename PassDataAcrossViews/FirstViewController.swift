c6c9fd096083203ae3337b444ffcd87a704a6609
bozsum <bozsumwv@gmail.com>
2018-11-01 00:45:08 +0700

    hide keyboard when press return on keyboard or touch anywhere outside textfield

--- PassDataAcrossViews/FirstViewController.swift
+++ PassDataAcrossViews/FirstViewController.swift
@@ -1,14 +1,28 @@
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var messageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageTextField.delegate = self
     }
-
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide keyboard
+        messageTextField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
+    @IBAction func hideKeyboard(_ sender: UITapGestureRecognizer) {
+        messageTextField.resignFirstResponder()
+    }
+    
     
     @IBAction func sendMessageButton(_ sender: UIButton) {
         
--- PassDataAcrossViews/SecondViewController.swift
+++ PassDataAcrossViews/SecondViewController.swift
@@ -1,14 +1,26 @@
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var messageTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageTextField.delegate = self
     }
     
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hide keyboard
+        messageTextField.resignFirstResponder()
+        
+        return true
+    }
+    
+    @IBAction func hideKeyboard(_ sender: UITapGestureRecognizer) {
+        messageTextField.resignFirstResponder()
+    }
     
     @IBAction func sendMessageButton(_ sender: UIButton) {
     
