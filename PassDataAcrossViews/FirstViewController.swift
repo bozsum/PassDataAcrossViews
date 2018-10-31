0951a00c023ae2c48695283ec032d90f41688b86
bozsum <bozsumwv@gmail.com>
2018-11-01 00:28:35 +0700

    connect ui to code

---
+++ PassDataAcrossViews/FirstViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func sendMessageButton(_ sender: UIButton) {
+        
+    }
+}
